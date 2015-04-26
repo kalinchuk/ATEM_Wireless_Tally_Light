31de5dba1a268d2cd2259fdfd0e559dddd760701
Artem Kalinchuk <artem@codegardenllc.com>
2015-04-25 20:17:57 -0400

    Update to support ATEM v6.3

--- libraries/ATEM/ATEM.h
+++ libraries/ATEM/ATEM.h
@@ -1,7 +1,7 @@
 /*
-Copyright 2012 Kasper Skårhøj, SKAARHOJ, kasperskaarhoj@gmail.com
+Copyright 2012-2014 Kasper Skårhøj, SKAARHOJ K/S, kasper@skaarhoj.com
 
-This file is part of the ATEM library for Arduino
+This file is part of the Blackmagic Design ATEM Client library for Arduino
 
 The ATEM library is free software: you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published by the 
@@ -16,48 +16,79 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along 
 with the ATEM library. If not, see http://www.gnu.org/licenses/.
 
+
+IMPORTANT: If you want to use this library in your own projects and/or products,
+please play a fair game and heed the license rules! See our web page for a Q&A so
+you can keep a clear conscience: http://skaarhoj.com/about/licenses/
+
+
 */
 
 
-/**
-  Version 1.0.2
-**/
 
 
 #ifndef ATEM_h
 #define ATEM_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  #include "WProgram.h"
-#endif
+#include "Arduino.h"
+#include "EthernetUdp.h"
 
 
-#include <EthernetUdp.h>
 
-#ifndef __arm__
-	#include <avr/pgmspace.h>
-#endif
+
+
+
+// Header bits:
+#define ATEM_headerCmd_AckRequest 0x1
+#define ATEM_headerCmd_HelloPacket 0x2
+#define ATEM_headerCmd_Resend 0x4
+#define ATEM_headerCmd_RequestNextAfter 0x8
+#define ATEM_headerCmd_Ack 0x10
+
+#define ATEM_maxInitPackageCount 24		// The maximum number of initialization packages. By observation on a 2M/E 4K this is 0-20 = 21. We allocate a bit more then...
+#define ATEM_packetBufferLength 96		// Size of packet buffer
+
+
 
 class ATEM
 {
   private:
-	EthernetUDP _Udp;			// Udp Object for communication, see constructor.
-	uint16_t _localPort; 		// local port to send from
-	IPAddress _switcherIP;		// IP address of the switcher
-	boolean _serialOutput;		// If set, the library will print status/debug information to the Serial object
+	EthernetUDP _Udp;					// UDP object for communication, see constructor.
+	uint16_t _localPort; 				// Default local port to send from. Preferably it's chosen randomly inside the class.
+	IPAddress _switcherIP;				// IP address of the switcher
+	boolean _serialOutput;				// If set, the library will print status/debug information to the Serial object
 
-	uint8_t _sessionID;					// Used internally for storing packet size during communication
-	uint16_t _lastRemotePacketID;		// The most recent Remote Packet Id from switcher
-	uint8_t _packetBuffer[96];   			// Buffer for storing segments of the packets from ATEM and creating answer packets.
-	uint16_t _cmdLength;					// Used when parsing packets
-	uint16_t _cmdPointer;					// Used when parsing packets
-
+	// ATEM Connection Basics
 	uint16_t _localPacketIdCounter;  	// This is our counter for the command packages we might like to send to ATEM
-	boolean _hasInitialized;  			// If true, the initial reception of the ATEM memory has passed and we can begin to respond during the runLoop()
+	boolean _initPayloadSent;  			// If true, the initial reception of the ATEM memory has passed and we can begin to respond during the runLoop()
+	uint8_t _initPayloadSentAtPacketId;	// The Remote Package ID at which point the initialization payload was completed.
+	boolean _hasInitialized;  			// If true, all initial payload packets has been received during requests for resent - and we are completely ready to rock!
+	boolean _isConnected;				// Set true if we have received a hello package from the switcher.
+	uint16_t _sessionID;				// Session id of session, given by ATEM switcher
 	unsigned long _lastContact;			// Last time (millis) the switcher sent a packet to us.
-	unsigned long _isConnectingTime;	// Set to millis() after the connect() function was called - and it will force runLoop() to finish the connection session.
+	uint16_t _lastRemotePacketID;		// The most recent Remote Packet Id from switcher
+	boolean _missedInitializationPackages[ATEM_maxInitPackageCount];	// Used to track which initialization packages have been missed
+	
+	// ATEM Buffer:
+	uint8_t _packetBuffer[ATEM_packetBufferLength];   		// Buffer for storing segments of the packets from ATEM and creating answer packets.
+
+	uint16_t _cmdLength;				// Used when parsing packets
+	uint16_t _cmdPointer;				// Used when parsing packets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		// Selected ATEM State values. Naming attempts to match the switchers own protocol names
 		// Set through _parsePacket() when the switcher sends state information
@@ -84,7 +115,12 @@ class ATEM
 	uint8_t _ATEM_MPClip[2];	// Clip number (if MPType==1)
 	uint16_t _ATEM_AMLv[2];	// Audio Meter Levels for a specific channel, see _ATEM_AMLv_channel
 	uint8_t _ATEM_AMLv_channel;		// The channel to read audio levels for.
-	uint8_t _ATEM_AudioChannelMode[13];	// Audio channel mode (ON=1, AFV=2, OFF=0/other)
+	uint8_t _ATEM_AudioChannelMode[16];	// Audio channel mode (ON=1, AFV=2, OFF=0/other)
+	uint8_t _ATEM_AudioChannelModeSpc[4];	// Audio channel mode for MP1, MP2, XLR and RCA
+	
+	
+	
+	
 	
 	
   public:
@@ -92,31 +128,44 @@ class ATEM
 	uint8_t	_ATEM_ver_m;	// Firmware version, "left of decimal point" (what is that called anyway?)
 	uint8_t	_ATEM_ver_l;	// Firmware version, decimals ("right of decimal point")
 
+
+
     ATEM();
     ATEM(const IPAddress ip, const uint16_t localPort);
-    void begin(const IPAddress ip, const uint16_t localPort);
+	void begin(const IPAddress ip);
+	void begin(const IPAddress ip, const uint16_t localPort);
     void connect();
+    void connect(const boolean useFixedPortNumber);
     void runLoop();
+	
+	uint16_t getATEM_lastRemotePacketId();
+	uint16_t getSessionID();
+	
+	bool isConnected();
+	bool hasInitialized();
 	bool isConnectionTimedOut();
-	void delay(const unsigned int delayTimeMillis);
 
   private:
+  	void createCommandHeader(const uint8_t headerCmd, const uint16_t lengthOfData);
+  	void createCommandHeader(const uint8_t headerCmd, const uint16_t lengthOfData, const uint16_t remotePacketID);
+  	void sendPacketBuffer(uint8_t length);
+	void _wipeCleanPacketBuffer();
+
+
 	void _parsePacket(uint16_t packetLength);
 	bool _readToPacketBuffer();
 	bool _readToPacketBuffer(uint8_t maxBytes);
-	void _sendAnswerPacket(uint16_t remotePacketID);
 	void _sendCommandPacket(const char cmd[4], uint8_t commandBytes[16], uint8_t cmdBytes);
-	void _wipeCleanPacketBuffer();
 	void _sendPacketBufferCmdData(const char cmd[4], uint8_t cmdBytes);
 
   public:
-
-/********************************
- * General Getter/Setter methods
- ********************************/
-  	void serialOutput(boolean serialOutput);
-	bool hasInitialized();
-	uint16_t getATEM_lastRemotePacketId();
+  	void serialOutput(uint8_t level);
+	bool hasTimedOut(unsigned long time, unsigned long timeout);
+	
+	
+	
+	
+	void delay(const unsigned int delayTimeMillis);
 	uint8_t getATEMmodel();
 
 /********************************
@@ -144,7 +193,7 @@ class ATEM
 	uint8_t getMediaPlayerStill(uint8_t mediaPlayer);
 	uint8_t getMediaPlayerClip(uint8_t mediaPlayer);
 	uint16_t getAudioLevels(uint8_t channel);
-	uint8_t getAudioChannelMode(uint8_t channelNumber);
+	uint8_t getAudioChannelMode(uint16_t channelNumber);
 
 /********************************
  * ATEM Switcher Change methods
@@ -154,6 +203,7 @@ class ATEM
 	void changePreviewInput(uint16_t inputNumber);
 	void doCut();
 	void doAuto();
+	void doAuto(uint8_t me);
 	void fadeToBlackActivate();
 	void changeTransitionPosition(word value);
 	void changeTransitionPositionDone();
@@ -186,6 +236,7 @@ class ATEM
 	void changeDVESettingsTemp_RunKeyFrame(uint8_t runType);
 	void changeDVESettingsTemp_Rate(uint8_t rateFrames);
 	void changeKeyerMask(uint16_t topMask, uint16_t bottomMask, uint16_t leftMask, uint16_t rightMask);
+	void changeKeyerMask(uint8_t keyer, uint16_t topMask, uint16_t bottomMask, uint16_t leftMask, uint16_t rightMask);
 	void changeDownstreamKeyMask(uint8_t keyer, uint16_t topMask, uint16_t bottomMask, uint16_t leftMask, uint16_t rightMask);
 	
 	void changeAudioChannelMode(uint16_t channelNumber, uint8_t mode);
@@ -200,4 +251,3 @@ class ATEM
 };
 
 #endif
-
