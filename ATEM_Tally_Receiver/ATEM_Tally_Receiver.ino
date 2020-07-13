449addbc26ae29d428ff36dfb7f11295c2510975
Magnus Franks <m.franks@live.dk>
2020-07-13 19:08:49 +0200

    Update ATEM_Tally_Receiver.ino
    - Corrected use of analogWrite() to digitalWrite()
    - Minor indentation fixes

--- ATEM_Tally_Receiver/ATEM_Tally_Receiver.ino
+++ ATEM_Tally_Receiver/ATEM_Tally_Receiver.ino
@@ -46,37 +46,37 @@ void setup() {
 	pinMode(DIP3_PIN, INPUT);
 	pinMode(DIP4_PIN, INPUT);
 
-	// turn all LEDs off (1023 is off)
-  analogWrite(PROGRAM_PIN, 1023);
-  analogWrite(PREVIEW_PIN, 1023);
-  analogWrite(POWER_PIN, 1023);  
+	// turn all LEDs off (HIGH is off)
+	digitalWrite(PROGRAM_PIN, HIGH);
+	digitalWrite(PREVIEW_PIN, HIGH);
+	digitalWrite(POWER_PIN, HIGH);  
   
 	// create an array of DIP pins
-  int dipPins[] = {DIP1_PIN, DIP2_PIN, DIP3_PIN, DIP4_PIN};
+	int dipPins[] = {DIP1_PIN, DIP2_PIN, DIP3_PIN, DIP4_PIN};
   
 	// set the Node # according to the DIP pins
-  setNodeID(dipPins, 4);
+	setNodeID(dipPins, 4);
   
 	if (this_node == 200) {
 		// if the Node # is 0 (alias to 200), blink quickly (30 times) on power on
 		for (int i=0; i < 30; i++) {
-			analogWrite(POWER_PIN, 0);
+			digitalWrite(POWER_PIN, LOW);
 			delay(20);
-			analogWrite(POWER_PIN, 1023);
+			digitalWrite(POWER_PIN, HIGH);
 			delay(20);
 		}
 	} else {
 		// if the Node # is a set number, blink the Node # on power on
 		for (int i=0; i < this_node; i++) {
-			analogWrite(POWER_PIN, 0);
+			digitalWrite(POWER_PIN, LOW);
 			delay(300);
-			analogWrite(POWER_PIN, 1023);
+			analogWrite(POWER_PIN, HIGH);
 			delay(300);
 		}
 	}
   
 	// set this variable to current time
-  last_radio_recv = millis();
+	last_radio_recv = millis();
 }
 
 void loop() {
@@ -86,27 +86,27 @@ void loop() {
 		// get the program and preview numbers
 		program_1 = rf12_data[0];
 		program_2 = rf12_data[2];
-    preview = rf12_data[4];
+   		preview = rf12_data[4];
 
 		// turn off all LEDs
-		digitalWrite(PREVIEW_PIN, 1023);
-		digitalWrite(PROGRAM_PIN, 1023);
-		digitalWrite(POWER_PIN, 1023);
+		digitalWrite(PREVIEW_PIN, HIGH);
+		digitalWrite(PROGRAM_PIN, HIGH);
+		digitalWrite(POWER_PIN, HIGH);
 
 		if (this_node == 200) {
 			// if the Node # is 200 (which is also 0), blink POWER LED every 1 second if signal exists
-			digitalWrite(POWER_PIN, 0);
+			digitalWrite(POWER_PIN, LOW);
 			delay(1000);
-			digitalWrite(POWER_PIN, 1023);
+			digitalWrite(POWER_PIN, HIGH);
 			delay(1000);
 		} else {
 			// if the Node # is a set number, trigger an LED accordingly
 			if (program_1 == this_node || program_2 == this_node) {
-				digitalWrite(PREVIEW_PIN, 1023);
-				analogWrite(PROGRAM_PIN, 0);
+				digitalWrite(PREVIEW_PIN, HIGH);
+				digitalWrite(PROGRAM_PIN, LOW);
 			} else if (preview == this_node) {
-				analogWrite(PROGRAM_PIN, 1023);
-				digitalWrite(PREVIEW_PIN, 0);
+				digitalWrite(PROGRAM_PIN, HIGH);
+				digitalWrite(PREVIEW_PIN, LOW);
 			}
 		}
 		
@@ -116,9 +116,9 @@ void loop() {
   
 	// turn off LEDs when no radio signal exists (the past 1 second)
 	if (millis() - last_radio_recv > 1000) {
-		digitalWrite(PREVIEW_PIN, 1023);
-		digitalWrite(PROGRAM_PIN, 1023);
-		digitalWrite(POWER_PIN, 1023);
+		digitalWrite(PREVIEW_PIN, HIGH);
+		digitalWrite(PROGRAM_PIN, HIGH);
+		digitalWrite(POWER_PIN, HIGH);
 	}
 }
 
@@ -132,11 +132,11 @@ byte setNodeID(int* dipPins, int numPins) {
 		}
 	}
 
-  this_node = (int)(j + 0.5);
+	this_node = (int)(j + 0.5);
 
 	// if the DIP switches are all OFF, assign 200 (alias to 0) to the Node #
-  this_node = this_node == 0 ? 200 : this_node;
+	this_node = this_node == 0 ? 200 : this_node;
 
 	// initialize the radio
-  rf12_initialize(this_node, RF12_433MHZ, 4);
+	rf12_initialize(this_node, RF12_433MHZ, 4);
 }
